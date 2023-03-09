9ff15ea218e2dd74d6ad6e8baf8d824b561b987d
Muthuraaj <87850296+Muthuraaj@users.noreply.github.com>
2023-03-09 18:23:42 +0530

    added icon collapse

--- index.js
+++ index.js
@@ -7,24 +7,37 @@
 //   document.querySelector("body").classList.toggle("sidemenus")
 // }
 
+//const side=document.getElementById('side1');
+
+//console.log(side.childNodes)
+// const sidemenus=document.getElementsByClassName('sidemenus')
+// for(var i=0;i<sidemenus.length;i++){
+//     sidemenus[i].style.backgroundColor="yellow";
+
+// }
+
+
 function openClose() {
   var elems = document.getElementsByClassName('navsec');
   const side=document.getElementById('side1');
+ 
 
-  
-    for (var i=0;i<elems.length;i+=1){
-      if (elems[i].style.display === "none") {
+    for (var i=0;i<elems.length;i++){
+      if (elems[i].style.display === "none")
+      {
         elems[i].style.display="inline";
-        elems[i].style.width = "20%";
+      
         side.style.width="20%";
 
-      } else {
+      } 
+      else 
+      {
         elems[i].style.display = "none";
-        side.style.width="10%";
+        side.style.width="5%";
 
         
       }
-    
+      
      
     
       

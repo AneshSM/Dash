6da4af92fef8abe8543567a147c20ef8c4dea7cb
Muthuraaj <87850296+Muthuraaj@users.noreply.github.com>
2023-03-15 17:28:14 +0530

    Added new files

--- index.js
+++ index.js
@@ -15,60 +15,81 @@
 //     sidemenus[i].style.backgroundColor="yellow";
 
 // }
+const side = document.getElementById('side1');
+var elems = document.getElementsByClassName('navsec');
+
+var winwidth, cltwidth;
+window.onresize = window.onload = () => {
+  winwidth = this.innerWidth;
+  cltwidth = this.document.clientWidth;
+if(winwidth<1000){
+  side.style.width='100%';
+  for (var i = 0; i < elems.length; i++){
+    elems[i].style.display='inline'
+  }
+}
+else{
+  openClose()
+}
+
+}
 
 
 function openClose() {
-  var elems = document.getElementsByClassName('navsec');
-  const side=document.getElementById('side1');
-  // const list=document.getElementById('dash')
- 
 
-    for (var i=0;i<elems.length;i++){
-      if (elems[i].style.display === "none")
-      {
-        side.style.width="20%";
-        elems[i].style.display="inline";
-        
-      } 
-      else 
-      {
-        elems[i].style.display = "none";
-        side.style.width="5%"; 
-        
-      }      
+  // const list=document.getElementById('dash')
+
+  if (side.style.width === "5%") {
+    side.style.width = "20%";
+
+    for (var i = 0; i < elems.length; i++) {
+      elems[i].style.display = "inline";
     }
-    
+  }
+
+  else {
+    side.style.width = "5%";
+    for (var i = 0; i < elems.length; i++) {
+      elems[i].style.display = "none";
+    }
+
+  }
 }
+
+
+
+
+
 
 
 
 // Tabs
 
-const tabs=document.getElementsByClassName('tabs');
+const tabs = document.getElementsByClassName('tabs');
 
 
 // for(i=0;i<tabs.length;i++){
 //   tabs[i].addEventListener('click',navigate)
 // }
 navigate()
-function navigate(status='dashboard'){
-  const navigator=document.getElementById(status);
+function navigate(status = 'dashboard') {
+  const navigator = document.getElementById(status);
 
-  for(i=0;i<tabs.length;i++){
-      tabs[i].style.borderLeft="none";
-      tabs[i].style.color="#FFFFFF";
-      tabs[i].style.backgroundColor="transparent";
-      // console.log(tabs[i]);
+  for (i = 0; i < tabs.length; i++) {
+    tabs[i].style.borderLeft = "none";
+    tabs[i].style.color = "#FFFFFF";
+    tabs[i].style.backgroundColor = "transparent";
+    // console.log(tabs[i]);
   }
   // navigator.style.borderLeft="3px solid #DEECF6";
-  navigator.style.color="#DEECF6";
-  navigator.style.backgroundColor="#3963AD";
-  navigator.style.borderRadius='5px';
-  const show=document.getElementsByClassName('showcase');
-  for(i=0;i<show.length;i++){
-    show[i].style.display='none';
+  navigator.style.color = "#DEECF6";
+  navigator.style.backgroundColor = "#3963AD";
+  navigator.style.borderRadius = '5px';
+  const show = document.getElementsByClassName('showcase');
+  for (i = 0; i < show.length; i++) {
+    show[i].style.display = 'none';
   }
-  document.getElementById(status+'_page').style.display='block';
+  document.getElementById(status + '_page').style.display = 'block';
 
 }
 
